8ce34ddf1790e0ba353f0a99a5ae39cfa6e867e5
Abhijeet <abhijeet.gotkhindi@gmail.com>
2025-07-05 14:15:12 +0530

    fom validation

--- src/services/schoolService.js
+++ src/services/schoolService.js
@@ -3,7 +3,7 @@ import { v6 as uuidv6 } from 'uuid';
 
 export const schoolList = async () => {
     try {
-        const [schools] = await pool.query('SELECT oid,ngo,schoolcode,schoolname,kschoolname,addressline1,addressline2,village,taluka,district,pincode,contactno,headmastername,headmasteremail,headmasterno,1std,2std,3std,4std,5std,6std,7std,8std,9std,10std,1std_female,2std_female,3std_female,4std_female,5std_female,6std_female,7std_female,8std_female,9std_female,10std_female,1std_male,2std_male,3std_male,4std_male,5std_male,6std_male,7std_male,8std_male,9std_male,10std_male FROM school WHERE status = 1');
+        const [schools] = await pool.query('SELECT oid,ngo,schoolcode,schoolname,kschoolname,addressline1,addressline2,village,taluka,district,pincode,route,contactno,headmastername,headmasteremail,headmasterno,1std,2std,3std,4std,5std,6std,7std,8std,9std,10std,1std_female,2std_female,3std_female,4std_female,5std_female,6std_female,7std_female,8std_female,9std_female,10std_female,1std_male,2std_male,3std_male,4std_male,5std_male,6std_male,7std_male,8std_male,9std_male,10std_male FROM school WHERE status = 1');
         if (schools.length === 0) {
             return { success: true, message: 'Data not found', schools: [] };
         }
@@ -21,7 +21,7 @@ export const schoolList = async () => {
 
 export const getSchoolById = async (schooloid) => {
     try {
-        const [schools] = await pool.query('SELECT oid,ngo,schoolcode,schoolname,kschoolname,addressline1,addressline2,village,taluka,district,pincode,contactno,headmastername,headmasteremail,headmasterno,1std,2std,3std,4std,5std,6std,7std,8std,9std,10std,1std_female,2std_female,3std_female,4std_female,5std_female,6std_female,7std_female,8std_female,9std_female,10std_female,1std_male,2std_male,3std_male,4std_male,5std_male,6std_male,7std_male,8std_male,9std_male,10std_male FROM school WHERE status = 1 AND oid IN (?)', [schooloid.split(',')]);
+        const [schools] = await pool.query('SELECT oid,ngo,schoolcode,schoolname,kschoolname,addressline1,addressline2,village,taluka,district,pincode,route,contactno,headmastername,headmasteremail,headmasterno,1std,2std,3std,4std,5std,6std,7std,8std,9std,10std,1std_female,2std_female,3std_female,4std_female,5std_female,6std_female,7std_female,8std_female,9std_female,10std_female,1std_male,2std_male,3std_male,4std_male,5std_male,6std_male,7std_male,8std_male,9std_male,10std_male FROM school WHERE status = 1 AND oid IN (?)', [schooloid.split(',')]);
         if (schools.length === 0) {
             return { success: true, message: 'Data not found', schools: [] };
         }
@@ -35,7 +35,7 @@ export const getSchoolById = async (schooloid) => {
 export const ngoSchoolList = async (ngooid) => {
     const { oid } = ngooid;
     try {
-        const [schools] = await pool.query('select oid,ngo,schoolcode,schoolname,kschoolname,addressline1,addressline2,village,taluka,district,pincode,contactno,headmastername,headmasteremail,headmasterno,1std,2std,3std,4std,5std,6std,7std,8std,9std,10std,1std_female,2std_female,3std_female,4std_female,5std_female,6std_female,7std_female,8std_female,9std_female,10std_female,1std_male,2std_male,3std_male,4std_male,5std_male,6std_male,7std_male,8std_male,9std_male,10std_male from school where status = 1 AND ngo IN (?)', [oid]);
+        const [schools] = await pool.query('select oid,ngo,schoolcode,schoolname,kschoolname,addressline1,addressline2,village,taluka,district,pincode,route,contactno,headmastername,headmasteremail,headmasterno,1std,2std,3std,4std,5std,6std,7std,8std,9std,10std,1std_female,2std_female,3std_female,4std_female,5std_female,6std_female,7std_female,8std_female,9std_female,10std_female,1std_male,2std_male,3std_male,4std_male,5std_male,6std_male,7std_male,8std_male,9std_male,10std_male from school where status = 1 AND ngo IN (?)', [oid]);
         if (schools.length === 0) {
             return { success: true, message: 'Data not found', schools: [] };
         }
@@ -51,7 +51,7 @@ export const ngoSchoolList = async (ngooid) => {
 export const insertSchool = async (datavalues) => {
     try {
         datavalues["uuid"] = uuidv6();
-        const [results] = await pool.query("INSERT INTO school (" + Object.keys(datavalues) + ") VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", Object.values(datavalues));
+        const [results] = await pool.query("INSERT INTO school (" + Object.keys(datavalues) + ") VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", Object.values(datavalues));
         return { success: true, message: "Inserted Records: " + results.affectedRows };
     } catch (error) {       
         writeLog(error);
@@ -61,7 +61,7 @@ export const insertSchool = async (datavalues) => {
 
 export const updateSchool = async (datavalues) => {
     try {
-        const [results] = await pool.query("UPDATE school  SET ngo = ?, schoolcode = ?,  schoolname = ?, kschoolname = ?, addressline1 = ?, addressline2 = ?, village = ?, taluka = ?, district = ?, pincode = ?, contactno = ?, headmastername = ?, headmasterno = ?, headmasteremail = ?, 1std_female = ?, 1std_male = ?, 1std = ?, 2std_female = ?, 2std_male = ?, 2std = ?, 3std_female = ?, 3std_male = ?, 3std = ?, 4std_female = ?, 4std_male = ?, 4std = ?, 5std_female = ?, 5std_male = ?, 5std = ?, 6std_female = ?, 6std_male = ?, 6std = ?, 7std_female = ?, 7std_male = ?, 7std = ?, 8std_female = ?, 8std_male = ?, 8std = ?, 9std_female = ?, 9std = ?, 10std_female = ?, 10std_male = ?, 10std = ?, updatedBy = ?, updatedtime = NOW() WHERE oid = ?", Object.values(datavalues));
+        const [results] = await pool.query("UPDATE school  SET ngo = ?, schoolcode = ?,  schoolname = ?, kschoolname = ?, addressline1 = ?, addressline2 = ?, village = ?, taluka = ?, district = ?, pincode = ?, route = ?, contactno = ?, headmastername = ?, headmasterno = ?, headmasteremail = ?, 1std_female = ?, 1std_male = ?, 1std = ?, 2std_female = ?, 2std_male = ?, 2std = ?, 3std_female = ?, 3std_male = ?, 3std = ?, 4std_female = ?, 4std_male = ?, 4std = ?, 5std_female = ?, 5std_male = ?, 5std = ?, 6std_female = ?, 6std_male = ?, 6std = ?, 7std_female = ?, 7std_male = ?, 7std = ?, 8std_female = ?, 8std_male = ?, 8std = ?, 9std_female = ?, 9std = ?, 10std_female = ?, 10std_male = ?, 10std = ?, updatedBy = ?, updatedtime = NOW() WHERE oid = ?", Object.values(datavalues));
         return { success: true, message: "Updated Records: " + results.affectedRows };
     } catch (error) {        
         writeLog(error);
